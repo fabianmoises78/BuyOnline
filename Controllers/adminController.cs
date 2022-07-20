93d77770636247669b87d9c4f9977706dcddf587
Moises Fabián Cruz <94415134+fabianmoises78@users.noreply.github.com>
2022-07-19 18:23:14 -0600

    añade clase controladora, sesion de admin y diseño de mantenimiento

---
+++ Controllers/adminController.cs
@@ -0,0 +1,75 @@
+﻿using BuyOnline.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BuyOnline.Controllers
+{
+    public class adminController : Controller
+    {
+        // GET: admin
+        Datos conexion = new Datos();
+
+        public ActionResult Mantenimiento()
+        {
+            return View();
+        }
+
+        public ActionResult LoginAdmin()
+        {
+            return View();
+        }
+
+        public ActionResult CRUDpais()
+        {
+            return View();
+        }
+
+        public ActionResult CRUDcliente()
+        {
+            return View();
+        }
+
+        public ActionResult CRUDadmin()
+        {
+            return View();
+        }
+
+        public ActionResult CRUDcategoria()
+        {
+            return View();
+        }
+
+        public ActionResult CRUDestados()
+        {
+            return View();
+        }
+
+        public ActionResult CRUDcompras()
+        {
+            return View();
+        }
+
+        public ActionResult CRUDdetallecompra()
+        {
+            return View();
+        }
+
+        public ActionResult CRUDproductos()
+        {
+            return View();
+        }
+
+        public ActionResult CRUDtarjeta()
+        {
+            return View();
+        }
+
+        public ActionResult CRUDtipopago()
+        {
+            return View();
+        }
+    }
+}
